ed8fccafb5316c7f12c8d5242fffd391f3e8e78d
Bárbara Arruda <65787282+barbaraarruda@users.noreply.github.com>
2021-09-16 12:07:38 -0300

    Update chromewindows.py

--- chromewindows.py
+++ chromewindows.py
@@ -5,3 +5,4 @@ chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe%s'
 driver = webdriver.Chrome(chrome_path)
 driver.get("https://open.spotify.com/playlist/6aKw3Z085NuSiK8wPvh7l3?si=81ab8662dede4712")
 
+#para mudar o navegador, só trocar o nome do chrome pelo navegador desejado. Observar também o endereço onde o navegador fica hospedado no windows.
